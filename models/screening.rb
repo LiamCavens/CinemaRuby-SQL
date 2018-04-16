19b17b84e383be79fb92983141c411726319246c
Liam Cavens <liamcavens@googlemail.com>
2018-04-16 09:19:54 +0100

    added another line of code

--- models/screening.rb
+++ models/screening.rb
@@ -67,8 +67,8 @@ class Screening
          end
     end
 
-    # def limit_customers_in_screen(film)
-    #     return "Film Sold Out" if film.get_customers_in_screen() > @screen_limit && film.get_customers_in_screen() > @screen_limit + 1
-    #         @customers_in_screen.drop(1)
-    # end
+    def limit_customers_in_screen(film)
+         return "Film Sold Out" if film.get_customers_in_screen() > @screen_limit && film.get_customers_in_screen() >= @screen_limit + 1
+             @customers_in_screen.drop(1)
+     end
 end
